21b37a1b54288066d7ee28e81b12ca70883b2b84
turk-sohail <ksohail470@gmail.com>
2024-11-25 17:00:18 +0500

    added validation type

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -1,18 +1,12 @@
 import { Module, ValidationPipe } from '@nestjs/common';
 import { APP_PIPE } from '@nestjs/core';
+import { VALIDATION_CONSTANTS } from './common.constants';
 
 @Module({
   providers: [
     {
       provide: APP_PIPE,
-      useValue: new ValidationPipe({
-        whitelist: true,
-        forbidNonWhitelisted: true,
-        transform: true,
-        transformOptions: {
-          enableImplicitConversion: true,
-        },
-      }),
+      useValue: new ValidationPipe(VALIDATION_CONSTANTS),
     },
   ],
 })
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsPhoneNumber, IsString } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export class CreateUserDto {
   @IsString()
