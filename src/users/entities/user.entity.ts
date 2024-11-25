a3ce5c199378b59cccfd9f6aee12abd6d1870498
turk-sohail <ksohail470@gmail.com>
2024-11-25 18:03:49 +0500

    added entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,7 +1,19 @@
+import { RegistryDates } from 'src/common/embedded/registry-dates.embedded';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
+  @Column()
   name: string;
+  @Column({ unique: true })
   email: string;
+  @Column({ unique: true })
   phone: string;
+  @Column()
   password: string;
+
+  @Column(({}) => RegistryDates, { prefix: false })
+  registryDates: RegistryDates;
 }
