c95134532d535b5fa60aba411e8467413a6ba3d2
turk-sohail <ksohail470@gmail.com>
2024-11-25 17:51:00 +0500

    added db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { CommonModule } from './common/common.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [UsersModule, CommonModule],
+  imports: [UsersModule, CommonModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/database/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      username: 'postgres',
+      password: 'postgres',
+      host: 'localhost',
+      port: 5444,
+      database: 'conrod',
+      autoLoadEntities: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
